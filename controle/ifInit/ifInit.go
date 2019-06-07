702da8fd34785cd9d7f891735e238b8de1fc71b5
rodrigoaraujoti <rodrigoaraujoeti@gmail.com>
2019-06-07 00:26:01 -0300

    Incluir exemplos da aula de controle > if init

---
+++ controle/ifInit/ifInit.go
@@ -0,0 +1,24 @@
+// AULA_025 - IF INIT
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func numeroAleatorio() int {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+	return r.Intn(10)
+}
+
+func main() {
+	if i := numeroAleatorio(); i > 5 {
+		fmt.Println("Ganhou!!!")
+		fmt.Println(i)
+	} else {
+		fmt.Println("Perdeu!")
+		fmt.Println(i)
+	}
+}
