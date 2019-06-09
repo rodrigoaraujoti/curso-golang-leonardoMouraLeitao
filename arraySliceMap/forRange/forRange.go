ccb878cbbb689163fceb716832fad0b1cfad7d8d
rodrigoaraujoti <rodrigoaraujoeti@gmail.com>
2019-06-08 23:12:21 -0300

    Adicionar novos comentarios ao exemplo arraysliceMap/forRange

--- arraySliceMap/forRange/forRange.go
+++ arraySliceMap/forRange/forRange.go
@@ -6,12 +6,19 @@ import "fmt"
 func main() {
 	numeros := [...]int{1, 2, 3, 4, 5} // o compilador conta!
 
+	fmt.Println("for index, item := range arraySlice")
 	for i, numero := range numeros {
 		fmt.Printf("%d) %d\n", i, numero)
 	}
 
 	fmt.Println("Exemplo de for range, com iterador ignerado, ao usar underscore")
+	fmt.Println("- O primeiro campo deste for eh sempre o indice, se ele nao for ser usado, informar o underscore")
 	for _, num := range numeros {
 		fmt.Println(num)
+	}
+
+	fmt.Println("- O segundo campo deste for eh sempre o item do range, se ele nao for ser usado, nao eh necessario informa-lo")
+	for i := range numeros {
+		fmt.Println(i)
 	}
 }
