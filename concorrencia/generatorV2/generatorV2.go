d5523fee48f1225be7790627af5118b51f9142aa
rodrigoaraujoti <rodrigoaraujoeti@gmail.com>
2019-06-12 01:19:11 -0300

    Criar exemplo de uma segunda implementacao para o capitulo sobre concorrencia, aula 74

---
+++ concorrencia/generatorV2/generatorV2.go
@@ -0,0 +1,50 @@
+// AULA_74 PADRAO DE CONCORRENCIA: GENERATOR
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"regexp"
+)
+
+// Google I/O 2012 - Go Concurrency Pattern - Rob Pike
+// funcao que encapsula a goroutine
+
+// <-chan - canal somente-leitura
+func titulo(urls ...string) <-chan string {
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) { //funcao anonima
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			// fmt.Println(reflect.TypeOf(r)) ~ obs: o vscode nao autocompleta este tipo: *regexp.Regexp
+			c <- r.FindStringSubmatch(string(html))[1] //--> [0] toda a regex, [1] soh o coringa: (.*?)
+
+		}(url) // executando funcao anonima
+	}
+	return c
+}
+
+func main() {
+	urls := []string{
+		"https://www.cod3r.com.br",
+		"https://www.google.com.br",
+		"https://www.amazon.com",
+		"https://www.youtube.com",
+	}
+
+	t1 := titulo(urls...)
+
+	fmt.Println("--- ranking")
+	for i := 0; i < cap(urls); i++ {
+		fmt.Println(i, <-t1)
+	}
+
+	fmt.Println("\n--- testes sobre tipo: <-chan string")
+	fmt.Println("TypeOf <-chan string", reflect.TypeOf(t1))
+	fmt.Println("cap of <-chan string", cap(t1))
+}
