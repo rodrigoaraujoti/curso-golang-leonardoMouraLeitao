c274c5fbbe97e8cc18e60dc8f80f910b70b51bab
rodrigoaraujoti <rodrigoaraujoeti@gmail.com>
2019-06-13 22:51:57 -0300

    remover funcao que foi tornada publica no projeto go_area

--- concorrencia/generatorV2/generatorV2.go
+++ concorrencia/generatorV2/generatorV2.go
@@ -3,31 +3,10 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"reflect"
-	"regexp"
+
+	"github.com/rodrigoaraujoti/go_area/htmlutil"
 )
-
-// Google I/O 2012 - Go Concurrency Pattern - Rob Pike
-// funcao que encapsula a goroutine
-
-// <-chan - canal somente-leitura
-func titulo(urls ...string) <-chan string {
-	c := make(chan string)
-	for _, url := range urls {
-		go func(url string) { //funcao anonima
-			resp, _ := http.Get(url)
-			html, _ := ioutil.ReadAll(resp.Body)
-
-			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
-			// fmt.Println(reflect.TypeOf(r)) ~ obs: o vscode nao autocompleta este tipo: *regexp.Regexp
-			c <- r.FindStringSubmatch(string(html))[1] //--> [0] toda a regex, [1] soh o coringa: (.*?)
-
-		}(url) // executando funcao anonima
-	}
-	return c
-}
 
 func main() {
 	urls := []string{
@@ -37,7 +16,7 @@ func main() {
 		"https://www.youtube.com",
 	}
 
-	t1 := titulo(urls...)
+	t1 := htmlutil.Titulo(urls...)
 
 	fmt.Println("--- ranking")
 	for i := 0; i < cap(urls); i++ {
